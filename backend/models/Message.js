249956cd4ae49cbd03d268cf0fb571cac1355900
Shravan Bhosale <bhosaleshravan26@gmail.com>
2026-01-16 16:48:29 +0530

    Add frontend files as normal folder

--- backend/models/Message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const MessageSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  message: String,
-});
-
-module.exports = mongoose.model("Message", MessageSchema);
--- backend/src/routes/contact.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/contact", (req, res) => {
-  res.json({ message: "Contact API working ✅" });
-});
-
-module.exports = router;
